5d6d02714408c8eb0a90e43fe6dc536f2d4373c6
Beardless-sheik <44676055+Beardless-sheik@users.noreply.github.com>
2019-06-12 13:20:22 +0200

    Update variables.js
    Completion of tasks

--- variables.js
+++ variables.js
@@ -1,1 +1,16 @@
 // you solution should be here
+
+/*Assignment 1 solution*/
+
+var birthYear=1990;
+var futureYear=2000;
+var age=futureYear-birthYear;
+console.log("My birth year is: "+birthYear+" and my future year is: "+futureYear);
+console.log("I will be: "+age+" or "+(age-1)+" depending on the year I was born");
+
+/*Assignment 2 solution*/
+var currentAge=20;
+var maximumAge=50;
+var estAmount=2;
+var result=(maximumAge-currentAge)*365.25*estAmount;
+console.log("You will need "+result+" to last you until the ripe old age of "+maximumAge);
